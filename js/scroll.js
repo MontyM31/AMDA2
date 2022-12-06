eaed9353b84cf8bd56f24b4d7d512bd7d77a2741
IgnacioAzpeitia <ignacio.azpeitia23@gmail.com>
2022-12-05 21:54:06 -0600

    AMDAL

---
+++ js/scroll.js
@@ -0,0 +1,7 @@
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+btnScrollToTop.addEventListener("click",function () {
+
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+
+});
